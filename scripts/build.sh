28e5ddb3368988725eda27ac9a632eb9fc5aa66d
Julien ETIENNE <julien.etienne@intuiquest.com>
2021-09-04 16:01:18 +0700

    fix path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 cd ./src && zip -x "**/.DS_Store" -r ../build/osmand-plugin-enduro.osf .
 
-git add ./build/osmand-plugin-enduro.osf
+git add ../build/osmand-plugin-enduro.osf
 git commit -m "update OSF"
 git push
