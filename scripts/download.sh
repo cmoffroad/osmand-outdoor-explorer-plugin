b66219425fbddc70076f15d39a4e34ac380c65e2
Julien ETIENNE <julien.etienne@intuiquest.com>
2022-07-11 22:26:27 +0700

    backup

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,10 @@
 YEAR=$(date -j -v-1d +%y)
 MONTH=$(date -j -v-1d +%m)
-DAY="00"
+DAY=${1:-00}
 FILE=Thailand_asia_${YEAR}_${MONTH}_${DAY}.obf
 SOURCE="https://download.osmand.net/download?aosmc=true&self=true&file=${FILE}.gz"
 TARGET="./obf/${FILE}"
+
+echo $SOURCE
 
 curl -k -o - $SOURCE | gunzip > $TARGET
