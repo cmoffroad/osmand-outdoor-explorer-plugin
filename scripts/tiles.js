cb5371c73c46151901009f5722cdb9d23c71bd85
Julien ETIENNE <julien.etienne@intuiquest.com>
2022-12-19 22:03:06 +0700

    update

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -4,5 +4,5 @@ const { processOBFs } = require('./lib');
 // const files = fs.readdirSync(dir).filter(f => f.match(country));
 // console.log(files)
 
-processOBFs(`./obf`, 'Thailand',  [ 19.070825827131095,99.03986245393754 ], 20, 20, [ 12, 13 ]);
+processOBFs(`./obf`, 'Thailand',  [ 19.070825827131095,99.03986245393754 ], 25, 20, [ 12, 13 ]);
 // processOBFs(`./obf`, 'Indonesia', [ -8.656135907033477, 121.07986792148543 ], 20, 8, [ 12, 13 ]);
