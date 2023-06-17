4eee64103d45bf1c55bb5a1396f7799f75afee64
Julien ETIENNE <julien.etienne@intuiquest.com>
2023-06-17 17:47:07 +0700

    update

--- scripts/snapshots.js
+++ scripts/snapshots.js
@@ -11,6 +11,4 @@ const zoom = 13;
 // processSnapshots('samoeng-south', zoom, 18.76203943805478, 98.67446927373197, 1920, 1080, obfs);
 // processSnapshots('mae-rim', zoom, 18.960233952694963,98.81782404378954, 1920, 1080, obfs);
 // processSnapshots('doi-mon-lan', zoom, 19.483556396738397,99.3281062839093, 1080, 1920, obfs);
-processSnapshots('chiang-dao', zoom, 19.311450194917143, 98.84769879771719, 1920, 1080, obfs);
-
-
+// processSnapshots('chiang-dao', zoom, 19.311450194917143, 98.84769879771719, 1920, 1080, obfs);
