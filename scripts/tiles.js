611a7487a6cda5dba7b2f1494a0073ca71e5df3a
Julien ETIENNE <julien.etienne@intuiquest.com>
2023-06-22 09:23:13 +0700

    fix

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -1,3 +1,3 @@
 const { processOBFs } = require('./lib');
 
-processOBFs(`./latest`, 'Thailand',  [ 19.070825827131095,99.03986245393754 ], 30, 20, [ 13 ]);
+processOBFs(`./latest`, 'Thailand',  [ 19.070825827131095,99.03986245393754 ], 30, 20, [ 12, 13 ]);
